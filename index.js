ca7aae9de0ca6e463dec59b80230258505ed7a3e
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-02-16 13:15:48 +0100

    created a JSON object

---
+++ index.js
@@ -0,0 +1,43 @@
+// top 10 movies JSON object
+let topTenMovies = [
+    {
+        title: 'The Godfather',
+        director: 'Francis Ford Coppola'
+    },
+    {
+        title: 'The Departed',
+        director: 'Martin Scorcese'
+    },
+    {
+        title: 'Road to Perdition',
+        director: 'Sam Mendes'
+    },
+    {
+        title: 'Heat',
+        director: 'Michael Mann'
+    },
+    {
+        title: 'Casino',
+        director: 'Martin Scorsese'
+    },
+    {
+        title: 'Goodfellas',
+        director: 'Martin Scorsese'
+    },
+    {
+        title: 'American Gangster',
+        director: 'Ridley Scott'
+    },
+    {
+        title: 'Reservoir Dogs',
+        director: 'Quentin Tarantino'
+    },
+    {
+        title: 'Pulp Fiction',
+        director: 'Quentin Tarantino'
+    },
+    {
+        title: 'Scarface',
+        director: 'Brian De Palma'
+    }
+];
