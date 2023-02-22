fc1fcf7d53bb3e74e140e29ae8f1be96d8ec7e4a
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-02-22 12:56:18 +0100

    created a genres JSON object

--- index.js
+++ index.js
@@ -68,6 +68,22 @@ let movies = [
     }
 ];
 
+// genres JSON object
+let genres = [
+    {
+        name: 'Drama',
+        description: 'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.[1] Drama of this kind is usually qualified with additional terms that specify its particular super-genre, macro-genre, or micro-genre,[2] such as soap opera, police crime drama, political drama, legal drama, historical drama, domestic drama, teen drama, and comedy-drama (dramedy).'
+    },
+    {
+        name: 'Thriller',
+        description: 'Thriller is a genre of fiction with numerous, often overlapping, subgenres, including crime, horror and detective fiction. Thrillers are characterized and defined by the moods they elicit, giving their audiences heightened feelings of suspense, excitement, surprise, anticipation and anxiety.'
+    },
+    {
+        name: 'Crime',
+        description: 'Crime films, in the broadest sense, is a film genre inspired by and analogous to the crime fiction literary genre. Films of this genre generally involve various aspects of crime and its detection. Stylistically, the genre may overlap and combine with many other genres, such as drama or gangster film,[1] but also include comedy, and, in turn, is divided into many sub-genres, such as mystery, suspense or noir.'
+    }
+];
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to your personal movie collection!');
