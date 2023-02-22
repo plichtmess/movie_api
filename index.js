ef91802fc6828add19237d5fea8640c3b2ad9c91
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-02-22 12:55:24 +0100

    required body-parser and uuid

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'),
-morgan = require('morgan');
+morgan = require('morgan'),
+bodyParser = require('body-parser'),
+uuid = require('uuid');
 
 const app = express();
 
