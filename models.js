d87b415783bbc3c2a6c8712dc6795b81b175ff22
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-02-27 15:23:43 +0100

    defined a user and genre model

--- models.js
+++ models.js
@@ -21,3 +21,18 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+let userSchema = mongoose.Schema({
+    // additional "required:" property = each user document must have a username field and value must be a string!
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    // key contains an array of IDs that each refer to a document within the “db.movies” collection. this is done by way of the ref attribute
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let genreSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Description: {type: String, required: true}
+});
+
