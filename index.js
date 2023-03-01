830fcba95fffdaccbb1c50e80b9e522d3a5b5eb8
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-03-01 14:59:51 +0100

    connected via mongoose to environmental variable

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const Genres = Models.Genre;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+//     useNewUrlParser: true, useUnifiedTopology: true
+// });
+
+mongoose.connect( process.env.CONNECTION_URI, {
     useNewUrlParser: true, useUnifiedTopology: true
 });
 
