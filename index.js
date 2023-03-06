cb5c3ed080ee7c034276197c780684bf02149270
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-03-06 12:40:31 +0100

    hashed the password for the user update endpoint

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ app.post('/users',
         }
 
     let hashedPassword = Users.hashPassword(req.body.Password);
+
     Users.findOne({ Username: req.body.Username }) // search to see if username already exists
     .then((user) => {
         if(user) {
@@ -212,11 +213,13 @@ app.put('/users/:Username',
          if (!errors.isEmpty()) {
              return res.status(422).json({ errors: errors.array() });
          }
+    
+    let hashedPassword = Users.hashPassword(req.body.Password);
 
     Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
         Username: req.body.Username,
-        Password: req.body.Password,
+        Password: hashedPassword,
         Email: req.body.Email,
         Birthday: req.body.Birthday
     }
