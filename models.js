6b35fd057588b79bb1521971d794b0b7fe4eb392
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-18 17:05:06 +0200

    Update models.js

--- models.js
+++ models.js
@@ -4,32 +4,32 @@ const mongoose = require('mongoose'),
 // defining the schema for movie and user documents to keep collections uniform
 let movieSchema = mongoose.Schema({
     // additional "required:" property = each user document must have a username field and value must be a string!
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
     // value is a subdocument containing keys for Name and Description/Bio, which have a value of String
-    genre: {
-        name: String,
-        description: String
+    Genre: {
+        Name: String,
+        Description: String
     },
-    director: {
-        name: String,
-        bio: String
+    Director: {
+        Name: String,
+        Bio: String
     },
     // key and an array, with the array containing values of a certain data type (all movie documents would have an actors key whose value would be an array of strings)
-    actors: [String],
+    Actors: [String],
     // simple key (field) value (data type) pair
-    imagePath: String,
-    featured: Boolean
+    ImagePath: String,
+    Featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
     // additional "required:" property = each user document must have a username field and value must be a string!
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    birthday: Date,
+    Username: { type: String, required: true, unique: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    Birthday: Date,
     // key contains an array of IDs that each refer to a document within the “db.movies” collection. this is done by way of the ref attribute
-    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 //bcrypt
