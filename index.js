7a5898eb40573f633b32711dea8c79a925f4ba45
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-18 10:54:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -78,16 +78,16 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
 });
 
 // get all user data
-// app.get('/users', (req, res) => {
-//     Users.find()
-//         .then((users) => {
-//             res.status(201).json(users);
-//         })
-//         .catch((err) => {
-//             console.error(err);
-//             res.status(500).send('Error: ' + err);
-//         });
-// });
+app.get('/users', (req, res) => {
+    Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 // get user data by username
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
