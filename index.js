efcb207cf51b625814cd9e689996777140621df6
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-19 15:54:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // CORS
 const cors = require('cors');
 
+// Enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 // allow all domains
 app.use(cors());
 
