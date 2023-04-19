f0bd7cbd7d751b5f929cd0fb51165ff45bbcb20d
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-19 12:37:52 +0200

    Revert "Update auth.js"
    This reverts commit 4d77c6d1902b4b961403bebbf9ac910269fc9bc8.

--- auth.js
+++ auth.js
@@ -1,12 +1,15 @@
 const jwtSecret = 'your_jwt_secret'; //this has to be the same key used in the JWTStrategy
+
 const jwt = require('jsonwebtoken'),
-    passport = require('passport');
+passport = require('passport');
 
 require('./passport'); // my local passport file
 
+
+
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
-        subject: user.username, // this is the username encoded in the JWT
+        subject: user.Username, // this is the username encoded in the JWT
         expiresIn: '7d', // this specifies the token will expire in 7 days
         algorithm: 'HS256' // this is the algorithm used to "sign" or encode the values of the JWT
     });
@@ -17,20 +20,20 @@ let generateJWTToken = (user) => {
 module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', { session: false },
-            (error, user, info) => {
-                if (error || !user) {
-                    return res.status(400).json({
-                        message: 'something is not right',
-                        user: user
-                    });
-                }
-                req.login(user, { session: false }, (error) => {
-                    if (error) {
-                        res.send(error);
-                    }
-                    let token = generateJWTToken(user.toJSON());
-                    return res.json({ user, token });
+        (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'something is not right',
+                    user: user
                 });
-            })(req, res);
+            }
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
     });
 };
